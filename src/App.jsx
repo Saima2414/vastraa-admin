70b3ae4768011f3e9cd36d3421842e356668eaea
Brad Traversy <me@bradtravesy.com>
2026-03-16 15:27:54 +0530

    added admin route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
+import Login from './components/Login'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Admin = () => {
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const currency = '$'
+
+const App = () => {
+
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
+
+  useEffect(()=>{
+    localStorage.setItem('token',token)
+  },[token])
+
   return (
-    <div style={{padding:"40px"}}>
-      <h1>Admin Dashboard</h1>
-      <p>Vastraa Admin Panel Working 🚀</p>
+    <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer />
+      {token === ""
+        ? <Login setToken={setToken} />
+        : <>
+          <Navbar setToken={setToken} />
+          <hr />
+          <div className='flex w-full'>
+            <Sidebar />
+            <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+              <Routes>
+                  <Route path='/admin' element={<Add token={token} />} />
+                  <Route path='/admin/add' element={<Add token={token} />} />
+                  <Route path='/admin/list' element={<List token={token} />} />
+                  <Route path='/admin/orders' element={<Orders token={token} />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      }
     </div>
-  );
-};
+  )
+}
 
-export default Admin;
+export default App
